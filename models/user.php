af13991f62d80e2e60737ec9c380a893ca1674ac
Jeremy Pease <JeremyAPease@aol.com>
2010-02-04 17:34:33 -0500

    Added after sace callback to update the aro for a user

--- models/user.php
+++ models/user.php
@@ -76,6 +76,26 @@ class User extends AppModel {
 			return array('Group' => array('id' => $data['User']['group_id']));
 		}
 	}
+	
+	/**    
+	 * After save callback
+	 *
+	 * Update the aro for the user.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	function afterSave($created) {
+			if (!$created) {
+				$parent = $this->parentNode();
+				$parent = $this->node($parent);
+				$node = $this->node();
+				$aro = $node[0];
+				$aro['Aro']['parent_id'] = $parent[0]['Aro']['id'];
+				$this->Aro->save($aro);
+			}
+	}
+
 
 }
 ?>
