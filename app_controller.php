79358593752d6b6c09ff55e342ad367b367c27ba
Jeremy Pease <JeremyAPease@aol.com>
2010-02-04 21:09:11 -0500

    allowing display actions

--- app_controller.php
+++ app_controller.php
@@ -32,18 +32,16 @@
  */
 
 class AppController extends Controller {
-    var $components = array('Acl', 'Auth', 'Session');
+	var $components = array('Acl', 'Auth', 'Session');
 
-    function beforeFilter() {
+	function beforeFilter() {
         //Configure AuthComponent
-        $this->Auth->authorize = 'actions';
-        $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
-        $this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'login');
-        $this->Auth->loginRedirect = array('controller' => 'posts', 'action' => 'add');
+		$this->Auth->authorize = 'actions';
+		$this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
+		$this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'login');
+		$this->Auth->loginRedirect = array('controller' => 'posts', 'action' => 'add');
 		$this->Auth->actionPath = 'controllers/';
-    }
-	
-
-
+		$this->Auth->allowedActions = array('display');
+	}
 }
 ?>
